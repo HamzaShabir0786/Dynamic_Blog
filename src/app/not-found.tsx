2175e7eedca3b567533ef11e2130aa01e617ed9c
Hamza Shabir <hamzii2205@gmail.com>
2025-01-09 22:46:49 +0500

    add not found file

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function notFound() {
+  return (
+    <div className="h-[84vh] bg-blue-300 flex justify-center items-center">
+      <div> 404 There is no such page exist here..</div>
+    </div>
+  );
+}
