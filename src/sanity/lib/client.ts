d111851539826c88e3abd540407aaac065a33190
Hamza Shabir <hamzii2205@gmail.com>
2025-01-14 02:39:20 +0500

    some change in client and .env file

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,7 +3,7 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: process.env.MY_API_VERSION,
+  apiVersion: process.env.MY_API_VERSION || "2024-12-30",
   token: process.env.MY_TOKEN_ACCESS,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 });
